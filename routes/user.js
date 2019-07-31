4686767323be261297d9597da2691e26811037a5
prprprprbiu <869720477@qq.com>
2019-08-01 01:03:49 +0800

    登录

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,9 @@ const User = require('../models/UserModel')
 
 // router.prefix('/api/user')
 
+/**注册接口
+ * /api/user/register
+ */
 router.post('/register', async (ctx, next) =>{
   const {username, password, role} = ctx.request.body
 
@@ -22,6 +25,50 @@ router.post('/register', async (ctx, next) =>{
   }
   
 })
+
+/**登录接口
+ * /api/user/login
+ */
+router.post('/login', async (ctx, next) =>{
+
+  const req = ctx.request.body;
+  if(!req.username || !req.password){
+    return ctx.body = {
+      code : '-1',
+      msg: '用户名或密码不能为空'
+    }
+  }else {
+    const data = await User.findOne({
+      where:{
+        username: req.username
+      }
+    })
+
+    if (data){
+      if (data.password === req.password){
+      ctx.status = 200
+      ctx.body = {
+        success : true,
+        msg: '登陆成功',
+        role:data.role
+      }
+      }else{
+        console.log('password wrong')
+        ctx.body = {
+          success: false,
+          msg: 'password wrong'
+        }
+      }
+
+    }
+  }
+
+
+
+})
+
+
+
 
 
 router.get('/', function (ctx, next) {
